2d06b9db1610d14c494a172994f23c341415adbe
Romain Francois <romain@purrple.cat>
2024-01-27 08:39:59 +0100

    + react[foo] and react[foo()]

--- R/react.R
+++ R/react.R
@@ -1,25 +1,60 @@
 #' Reactivity helper
 #'
-#' The `react` object allow you to use the `react$foo` syntax
-#' instead of calling `foo()`.
+#' The `react` object gives alternative syntax to call
+#' shiny reactive expressions.
 #'
-#' The benefit is that it makes it easier to identify calls to reactive
-#' expressions and take them apart from calls to functions.
+#' The benefit is that it makes them easier to spot in your code
 #'
+#' @examples
+#' \dontrun{
+#'   # imagine you have some shiny reactive
+#'   foo <- reactive({
+#'     whatever() + something()
+#'   })
+#'
+#'   # react allows you to call it in 3 different ways
+#'   # instead of the usual foo() form
+#'   react$foo
+#'   react[foo]
+#'   react[foo()]
+#' }
 #' @export
 react <- structure(list(), class = "react")
 
+`%or%` <- function(x, y) {
+  tryCatch(x, error = function(e) y)
+}
+
 #' @export
 `$.react` <- function(x, name) {
-  eval(get(substitute(name), parent.frame())(), parent.frame())
+  get(substitute(name), parent.frame())()
+}
+
+#' @importFrom rlang abort
+#' @export
+`[.react` <- function(x, i) {
+  expr <- substitute(i)
+  frame <- parent.frame()
+  reactive <- switch(typeof(expr),
+    symbol = get(as.character(expr, frame)),
+    language = get(as.character(expr[[1L]], frame)),
+
+    abort(c(
+      "Unsupported react[<i>] form",
+      i = "Use react[foo] or react[foo()]"
+    ))
+  )
+
+  reactive()
 }
 
 #' @export
 print.react <- function(x, ...) {
+  code <- function(txt) {
+    cli::col_silver(cli::style_italic(txt))
+  }
   writeLines(glue::glue(
-    "\U1F4A1 : use {y} instead of {z} to call the foo reactive.",
-    y = cli::col_silver(cli::style_italic("react$foo")),
-    z = cli::col_silver(cli::style_italic("foo()"))
+    "\U1F4A1 : use {code('react$foo')}, {code('react[foo]')}, or {code('react[foo()]')} instead of {code('foo()')} to call the foo reactive."
   ))
 
   writeLines("\U1F914 : so that you can easily recognise reactive calls")
